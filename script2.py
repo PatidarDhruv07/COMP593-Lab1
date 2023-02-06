696b9296c1663e19d91554b1f991052c5a996bf9
Dhruvkumar Patel <122956998+PatidarDhruv07@users.noreply.github.com>
2023-02-06 12:58:27 -0500

    Modified both file in repo clone

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 #Simple is better than complex
 print("Namespaces are one honking great idea -- let's do more of those!")
+print("Now is better than never.")
+print("If the implementation is easy to explain, it may be a good idea.")
